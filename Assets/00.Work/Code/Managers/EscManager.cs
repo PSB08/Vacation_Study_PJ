4ee4d91727f055134a7ac2d9e940910ded8e3c6a
PSB08 <Parkseongbin08@gmail.com>
2026-08-19 22:37:05 +0900

    feat : EscManager, fix : Project setting

---
+++ Assets/00.Work/Code/Managers/EscManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Work.Code.Managers
+{
+    public class EscManager : MonoBehaviour
+    {
+        public void Exit()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+        
+    }
+}
